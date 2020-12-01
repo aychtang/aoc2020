1e4a21f6cd3322ff29d6ef0bead3cab957dda90e
Howard Tang <howard@tray.io>
2020-12-01 21:46:05 +0000

    Add file reading

--- p01.pl
+++ p01.pl
@@ -1,3 +1,13 @@
+:- use_module(library(pure_input)).
+:- use_module(library(dcg/basics)).
+
+line([X|Data]) --> integer(X), "\n", line(Data).
+line([]) --> eos.
+
+load_data(Data) :-
+	open('input.txt', read, Stream),
+	phrase_from_stream(line(Data), Stream).
+
 sum_to_target(List, Target, N1, N2) :-
 	select(N1, List, List2),
 	select(N2, List2, _),
@@ -9,10 +19,12 @@ sum_to_target(List, Target, N1, N2, N3) :-
 	select(N3, List3, _),
 	Target is N1 + N2 + N3.
 
-solution1(List, Target, Answer) :-
+solution1(Target, Answer) :-
+	load_data(List),
 	sum_to_target(List, Target, N1, N2),
 	Answer is N1 * N2.
 
-solution2(List, Target, Answer) :-
+solution2(Target, Answer) :-
+	load_data(List),
 	sum_to_target(List, Target, N1, N2, N3),
 	Answer is N1 * N2 * N3.
