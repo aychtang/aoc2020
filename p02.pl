30961908b9adb777cc326765a8cbdfb9920154d2
Howard Tang <howard@tray.io>
2020-12-03 20:51:58 +0000

    tabs are categorically better

--- p02.pl
+++ p02.pl
@@ -2,15 +2,15 @@
 :- use_module(library(dcg/basics)).
 
 line([[Minimum,Maximum,Character,Password]|Data]) -->
-    integer(Minimum),
-    "-",
-    integer(Maximum),
-    " ",
-    string(Character),
-    ": ",
-    string(Password),
-    "\n",
-    line(Data).
+	integer(Minimum),
+	"-",
+	integer(Maximum),
+	" ",
+	string(Character),
+	": ",
+	string(Password),
+	"\n",
+	line(Data).
 
 line([]) --> eos.
 
@@ -21,22 +21,22 @@ load_data(Data) :-
 occurrences_of(List, X, Count) :- aggregate_all(count, member(X, List), Count).
 
 occurs_at_positions_once(List, X, Pos1, Pos2) :-
-    nth1(Pos1, List, X), not(nth1(Pos2, List, X));
-    nth1(Pos2, List, X), not(nth1(Pos1, List, X)).
+	nth1(Pos1, List, X), not(nth1(Pos2, List, X));
+	nth1(Pos2, List, X), not(nth1(Pos1, List, X)).
 
 valid_password([Minimum, Maximum, [Code], CodeList]) :-
-    occurrences_of(CodeList, Code, Count),
-    Count >= Minimum,
-    Count =< Maximum.
+	occurrences_of(CodeList, Code, Count),
+	Count >= Minimum,
+	Count =< Maximum.
 
 valid_password_2([Pos1, Pos2, [Code], CodeList]) :-
-    occurs_at_positions_once(CodeList, Code, Pos1, Pos2).
+	occurs_at_positions_once(CodeList, Code, Pos1, Pos2).
 
 count_valid_passwords(Predicate, List, Count) :-
-    exclude(Predicate, List, List2),
-    length(List, Count1),
-    length(List2, Count2),
-    Count is Count1 - Count2.
+	exclude(Predicate, List, List2),
+	length(List, Count1),
+	length(List2, Count2),
+	Count is Count1 - Count2.
 
 solution(1, Answer) :-
 	load_data(List),
--- p03.pl
+++ p03.pl
@@ -4,7 +4,7 @@
 :- use_module(library(dcg/basics)).
 
 line([Line|Data]) -->
-    string(Line), "\n", line(Data).
+	string(Line), "\n", line(Data).
 
 line([]) --> eos.
 
@@ -13,31 +13,31 @@ load_data(Data) :-
 	phrase_from_stream(line(Data), Stream).
 
 check_collision_for_line(Line, N, Dx) :-
-    length(Line, LineLength),
-    Pos is (N * Dx) mod LineLength,
-    nth0(Pos, Line, 35).
+	length(Line, LineLength),
+	Pos is (N * Dx) mod LineLength,
+	nth0(Pos, Line, 35).
 
 lets_crash_into_some_trees([], _, _, _, _, 0).
-lets_crash_into_some_trees(_, Index, _, _, MaxY, 0) :- Index >= MaxY.
+lets_crash_into_some_trees(_, Index, _, Dy, _, 0) :- Index mod Dy #= 0.
 lets_crash_into_some_trees([Line|Tail], Index, Dx, Dy, MaxY, Count) :-
-    NextIndex is Index + Dy,
-    lets_crash_into_some_trees(Tail, NextIndex, Dx, Dy, MaxY, Acc),
-    (check_collision_for_line(Line, Index, Dx) ->
-        Count is Acc + 1;
-        Count is Acc
-    ).
+	NextIndex is Index + 1,
+	lets_crash_into_some_trees(Tail, NextIndex, Dx, Dy, MaxY, Acc),
+	(check_collision_for_line(Line, Index, Dx) ->
+		Count is Acc + 1;
+		Count is Acc
+	).
 
 solution(1, Answer) :-
 	load_data(List),
-    length(List, MaxY),
-    lets_crash_into_some_trees(List, 0, 3, 1, MaxY, Answer).
+	length(List, MaxY),
+	lets_crash_into_some_trees(List, 0, 3, 1, MaxY, Answer).
 
 solution(2, Answer) :-
 	load_data(List),
-    length(List, MaxY),
-    lets_crash_into_some_trees(List, 0, 1, 1, MaxY, A1),
-    lets_crash_into_some_trees(List, 0, 3, 1, MaxY, A2),
-    lets_crash_into_some_trees(List, 0, 5, 1, MaxY, A3),
-    lets_crash_into_some_trees(List, 0, 7, 1, MaxY, A4),
-    lets_crash_into_some_trees(List, 0, 1, 2, MaxY, A5),
-    Answer is A1 * A2 * A3 * A4 * A5.
+	length(List, MaxY),
+	lets_crash_into_some_trees(List, 0, 1, 1, MaxY, A1),
+	lets_crash_into_some_trees(List, 0, 3, 1, MaxY, A2),
+	lets_crash_into_some_trees(List, 0, 5, 1, MaxY, A3),
+	lets_crash_into_some_trees(List, 0, 7, 1, MaxY, A4),
+	lets_crash_into_some_trees(List, 0, 1, 2, MaxY, A5),
+	Answer is A1 * A2 * A3 * A4 * A5.
