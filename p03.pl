40827682cb01a4b3424af732c1ef2bf067def514
Howard Tang <howard@tray.io>
2020-12-03 21:46:48 +0000

    3 pt 2

--- p03.pl
+++ p03.pl
@@ -1,43 +1,43 @@
-:- set_prolog_flag(answer_write_options,[max_depth(0)]).
-:- use_module(library(lists)).
 :- use_module(library(pure_input)).
 :- use_module(library(dcg/basics)).
 
-line([Line|Data]) -->
-	string(Line), "\n", line(Data).
-
+line([Line|Data]) --> string(Line), "\n", line(Data).
 line([]) --> eos.
 
 load_data(Data) :-
 	open('input3.txt', read, Stream),
 	phrase_from_stream(line(Data), Stream).
 
-check_collision_for_line(Line, N, Dx) :-
-	length(Line, LineLength),
-	Pos is (N * Dx) mod LineLength,
-	nth0(Pos, Line, 35).
+check_collision_for_coord(Line, X, Y) :-
+	nth0(Y, Line, Row),
+	length(Row, Line_length),
+	Pos is X mod Line_length,
+	nth0(Pos, Row, 35).
 
-lets_crash_into_some_trees([], _, _, _, _, 0).
-lets_crash_into_some_trees(_, Index, _, Dy, _, 0) :- Index mod Dy > 0.
-lets_crash_into_some_trees([Line|Tail], Index, Dx, Dy, MaxY, Count) :-
-	NextIndex is Index + 1,
-	lets_crash_into_some_trees(Tail, NextIndex, Dx, Dy, MaxY, Acc),
-	(check_collision_for_line(Line, Index, Dx) ->
-		Count is Acc + 1;
-		Count is Acc
-	).
+lets_crash_into_some_trees(Map, _, Y, _, _, 0) :-
+	length(Map, MaxY),
+	Y >= MaxY.
+
+lets_crash_into_some_trees(Map, X, Y, Dx, Dy, Count) :-
+	(
+		check_collision_for_coord(Map, X, Y) ->
+			Count1 is 1;
+			Count1 is 0
+	),
+	X1 is X + Dx,
+	Y1 is Y + Dy,
+	lets_crash_into_some_trees(Map, X1, Y1, Dx, Dy, Acc),
+	Count is Count1 + Acc.
 
 solution(1, Answer) :-
-	load_data(List),
-	length(List, MaxY),
-	lets_crash_into_some_trees(List, 0, 3, 1, MaxY, Answer).
+	load_data(Map),
+	lets_crash_into_some_trees(Map, 0, 0, 3, 1, Answer).
 
 solution(2, Answer) :-
-	load_data(List),
-	length(List, MaxY),
-	lets_crash_into_some_trees(List, 0, 1, 1, MaxY, A1),
-	lets_crash_into_some_trees(List, 0, 3, 1, MaxY, A2),
-	lets_crash_into_some_trees(List, 0, 5, 1, MaxY, A3),
-	lets_crash_into_some_trees(List, 0, 7, 1, MaxY, A4),
-	lets_crash_into_some_trees(List, 0, 1, 2, MaxY, A5),
-	Answer is A1 * A2 * A3 * A4 * A5.
+	load_data(Map),
+	lets_crash_into_some_trees(Map, 0, 0, 1, 1, C1),
+	lets_crash_into_some_trees(Map, 0, 0, 3, 1, C2),
+	lets_crash_into_some_trees(Map, 0, 0, 5, 1, C3),
+	lets_crash_into_some_trees(Map, 0, 0, 7, 1, C4),
+	lets_crash_into_some_trees(Map, 0, 0, 1, 2, C5),
+	Answer is C1 * C2 * C3 * C4 * C5.
